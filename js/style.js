5f7615ff04e982fd0bad93db5f90ef7172a694b0
OdalysMendez12 <106688565+OdalysMendez12@users.noreply.github.com>
2023-06-16 09:08:32 -0600

    Add cambios banners
    banners

--- js/style.js
+++ js/style.js
@@ -1,4 +1,49 @@
 /*header*/
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+}
+/*
 const miBanner = document.querySelectorAll('.miBanner'),
 dot = document.querySelectorAll('.dot');
 
@@ -50,4 +95,20 @@ dot[counter - 1].classList.add('active');
 
 function slidefun(n){
 }
+<<<<<<< Updated upstream
 
+=======
+*/
+
+/*login*/
+function login() {
+    var user, password;
+    user = document.getElementById("user").value;
+    password = document.getElementById("password").value;
+    if (user == "prueba" && password == "12345") {
+        window.location = "perfilUsuario.html";
+    } else {
+        alert("Los datos ingresados son incorrectos, favor de verificar.");
+    }
+}
+>>>>>>> Stashed changes
