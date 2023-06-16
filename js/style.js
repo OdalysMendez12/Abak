d2693ff90633b91470a8bc8f5897725b1405e1c8
OdalysMendez12 <106688565+OdalysMendez12@users.noreply.github.com>
2023-06-15 19:30:00 -0600

    Add vistas inicio
    Banners jeje

--- js/style.js
+++ js/style.js
@@ -1,13 +1,56 @@
 /*header*/
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $("header").addClass("header2");
-        } else {
-            $("header").removeClass("header2");
-        }
-    });
-});
+const miBanner = document.querySelectorAll('.miBanner'),
+dot = document.querySelectorAll('.dot');
+
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoslide, 8000);
+
+function autoslide(){
+counter += 1;
+slidefun(counter);
+}
+
+function plusSlides(n){
+    counter += n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function currentSlide(n){
+    counter = n;
+    slidefun(counter);
+    resetTimer();
+}
+
+function resetTimer(){
+    clearInterval(timer);
+    timer = serInterval(autoslide, 8000);
+}
+
+function slidefun(n){
+    let i;
+    for (i = 0; i < miBanner.length; i++){
+        miBanner[i].style.display = "none";
+    }
+    for (i = 0; i < dot.length; i++){
+        dot[i].classList.remove('active');
+}
+
+if(n > miBanner.length){
+    counter = 1;
+}
+if(n < 1){
+    counter = miBanner.length;
+}
+miBanner[counter - 1].style.display = "block";
+dot[counter - 1].classList.add('active');
+}
+
+function slidefun(n){
+}
+
 
 /*login*/
 function login() {
