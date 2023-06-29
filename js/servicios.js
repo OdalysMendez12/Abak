f514aca54e5299154b0404bb28ee3564d376968d
OdalysMendez12 <106688565+OdalysMendez12@users.noreply.github.com>
2023-06-29 00:16:50 -0600

    Add slider servicios

--- js/servicios.js
+++ js/servicios.js
@@ -1,1 +1,15 @@
-/*DESCARGAR LA VERSIÓN 8.1.6 DE SWIPER*/
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
